0b5daf9904974781ac95e7d31894c133cc4882d3
mohannad-tawaijri <80490747+mohannad-tawaijri@users.noreply.github.com>
2022-06-14 06:58:32 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="bootstrap.css">
+
+
+
+
+
+    <title>Personal Website</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <div class="scroll-up-btn">
+        <i class="fas fa-angle-up"></i>
+    </div>
+    <nav class="navbar">
+        <div class="max-width">
+            <div class="logo"><a href="#"><span></span></a></div>
+            <ul class="menu">
+                <li><a href="#home" class="menu-btn">Home</a></li>
+                <li><a href="#about" class="menu-btn">About</a></li>
+                <li><a href="#services" class="menu-btn">Projects</a></li>
+                <li><a href="#skills" class="menu-btn">Skills</a></li>
+                <li><a href="#contact" class="menu-btn">Contact</a></li>
+            </ul>
+            <div class="menu-btn">
+                <i class="fas fa-bars"></i>
+            </div>
+        </div>
+    </nav>
+
+    <!-- home section start -->
+    <section class="home" id="home">
+        <div class="max-width">
+            <div class="home-content">
+                <div class="text-1">Hello, my name is</div>
+                <div class="text-2">Mohannad Altawaijri</div>
+                <div class="text-3">And I'm a <span class="typing"></span></div>
+                <a href="#">Hire me</a>
+            </div>
+        </div>
+    </section>
+
+    <!-- about section start -->
+    <section class="about" id="about">
+        <div class="max-width">
+            <h2 class="title">About me</h2>
+            <div class="about-content">
+                <div class="column left">
+                   
+                </div>
+                <div class="column right">
+                    <div class="text">I'm Mohannad Altawaijri and I'm a <span class="typing-2"></span></div>
+                    <p>I am a computer science student interested in web design and front end developer and focus on crafting clean and easy to use experiences, I am passionate about building excellent software that improves the lives of those around me</p>
+                    <a href="https://docs.google.com/uc?export=download&id=1AteKMyIkozNbSUjgMZlbRk2OIPDZbMl4">Download CV</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Projects section start -->
+    <section class="services" id="services">
+        <div class="max-width">
+            <h2 class="title">My Projects</h2>
+            <div class="serv-content">
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-paint-brush"></i>
+                        <div class="text"><a href="https://github.com/mohannad-tawaijri/Web-Quizzing-System-for-General-Knowledge">Web Quizzing System </a></div>
+                        <p>•	Presented pages of questions and answers for different subjects.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-chart-line"></i>
+                        <div class="text"><a href="https://github.com/mohannad-tawaijri/coffe-shop">coffee-shop </a></div>
+                        <p>•	A simple coffee shop website that allows you to order breakfast or coffee.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-code"></i>
+                        <div class="text"><a href="https://github.com/mohannad-tawaijri/registration-form">registration-form </a></div>
+                        <p>•	A website that is a registration form.</p>
+                    </div>
+                    </div>
+                </div>
+               </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- skills section start -->
+    <section class="skills" id="skills">
+        <div class="max-width">
+            <h2 class="title">My skills</h2>
+            <div class="skills-content">
+                
+                <div class="column right">
+                    <div class="bars">
+                        <div class="info">
+                            <span>HTML</span>
+                            <span>90%</span>
+                        </div>
+                        <div class="line html"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>CSS</span>
+                            <span>60%</span>
+                        </div>
+                        <div class="line css"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>JavaScript</span>
+                            <span>55%</span>
+                        </div>
+                        <div class="line js"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>PHP</span>
+                            <span>70%</span>
+                        </div>
+                        <div class="line php"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>MySQL</span>
+                            <span>70%</span>
+                        </div>
+                        <div class="line mysql"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>Python</span>
+                            <span>85%</span>
+                        </div>
+                        <div class="line python"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    
+    
+
+    <!-- contact section start -->
+    <section class="contact" id="contact">
+        <div class="max-width">
+            <h2 class="title">Contact me</h2>
+            <div class="contact-content">
+                <div class="column left">
+                    <div class="text">Get in Touch</div>
+                    <p>Feel free to get in touch with me. I am always open to discussing new projects, creative ideas or opportunities to be part of your visions.</p>
+                    <div class="icons">
+                        <div class="row">
+                            <i class="fas fa-user"></i>
+                            <div class="info">
+                                <div class="head">Name</div>
+                                <div class="sub-title">Mohannad Altawaijri</div>
+                            </div>
+                        </div>
+                        
+                        <div class="row">
+                            <i class="fa fa-twitter"></i>
+                            <div class="info">
+                                <div class="head">Twitter</div>
+                                <div class="sub-title">@Mohannad9924</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fa fa-github"></i>
+                            <div class="info">
+                                <div class="head">GitHub</div>
+                                <div class="sub-title">mohannad-tawaijri</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fa fa-linkedin-square"></i>
+                            <div class="info">
+                                <div class="head">linkedin</div>
+                                <div class="sub-title">Mohannad Altawaijri</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-envelope"></i>
+                            <div class="info">
+                                <div class="head">Email</div>
+                                <div class="sub-title">mohannad9926@gmail.com</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+               <div class="column right">
+                <?php 
+                $Msg = "";
+                if(isset($_GET['error']))
+                {
+                    $Msg = " Please Fill in the Blanks ";
+                    echo '<div class="alert alert-danger">'.$Msg.'</div>';
+                }
+
+                if(isset($_GET['success']))
+                {
+                    $Msg = " Your Message Has Been Sent ";
+                    echo '<div class="alert alert-success">'.$Msg.'</div>';
+                }
+            
+            ?>
+                    <div class="text">Message me</div>
+                    <form action="process.php" method="post">
+                        <div class="fields">
+                            <div class="field name">
+                                <input type="text" name="UName" placeholder="User Name" class="form-control mb-2">
+                            </div>
+                            <div class="field email">
+                                <input type="email" name="Email" placeholder="Email" class="form-control mb-2">
+                            </div>
+                        </div>
+                        <div class="field">
+                            <input type="text" name="Subject" placeholder="Subject" class="form-control mb-2">
+                        </div>
+                        <div class="field textarea">
+                            <textarea name="msg" class="form-control mb-2" placeholder="Write The Message"></textarea>
+                        <div class="button-area">
+                            <button class="btn btn-success" name="btn-send"> Send </button>
+                        </div>
+                    </form>
+                </div>
+                    
+                </div>
+            </div>
+        </div>
+
+      
+
+    </section>
+
+    <!-- footer section start -->
+    <footer>
+        <span>Created By <a href="https://github.com/mohannad-tawaijri">Mohannad Altawaijri</a> | <span class="far fa-copyright"></span> 2022 All rights reserved.</span>
+    </footer>
+
+    <script src="script.js"></script>
+</body>
+</html>
